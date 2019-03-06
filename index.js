201424abe6476f2a93ba2d7a1aa2acd119f4c7bd
Lucie <46979603+lucievr@users.noreply.github.com>
2019-03-06 23:41:46 +0100

    Add change colour to DOM event

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ document.querySelectorAll(".color-box")[0].addEventListener("click", function ()
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let a = (Math.random()).toFixed(2);
-    let rgbaColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + " )";
+    let rgbaColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
     
     document.querySelectorAll(".color-box")[0].style.backgroundColor  = rgbaColor;
     document.querySelectorAll(".color-code")[0].innerHTML = rgbaColor;
+    document.querySelectorAll(".color-code")[0].style.color = rgbaColor;
 });
 
 
 function blink() {
     let clickAbove = document.querySelectorAll(".click")[0];
     setInterval(function() {
-       clickAbove.style.display = (clickAbove.style.display == 'none' ? '' : 'none');
+       clickAbove.style.display = (clickAbove.style.display == "none" ? "" : "none");
     }, 1000);
  }
 
