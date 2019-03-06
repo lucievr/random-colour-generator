f1b546004ba853f6eaffbafeef1b2f09bb974a78
Lucie <46979603+lucievr@users.noreply.github.com>
2019-03-06 21:33:08 +0100

    Add event listener and text animation

---
+++ index.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+document.querySelectorAll(".color-box")[0].addEventListener("click", function () {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let a = (Math.random()).toFixed(2);
+    let rgbaColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + " )";
+    
+    document.querySelectorAll(".color-box")[0].style.backgroundColor  = rgbaColor;
+    document.querySelectorAll(".color-code")[0].innerHTML = rgbaColor;
+});
+
+
+function blink() {
+    let clickAbove = document.querySelectorAll(".click")[0];
+    setInterval(function() {
+       clickAbove.style.display = (clickAbove.style.display == 'none' ? '' : 'none');
+    }, 1000);
+ }
+
+blink ();
